808260b3df858956f295f8cda4a977b2e8b29df0
Christian Donnerer <christian.donnerer@gmail.com>
2019-12-01 16:47:03 +0000

    quick and dirty solution to day 1. could have used recursion in part2?

---
+++ day1/fuel_sum.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def calculate_fuel(mass):
+    """Divide mass by three, round down and subtract 2
+    """
+    return int(mass / 3) - 2
+    
+def load_masses(path='input.dat'):
+    return np.loadtxt(path)
+    
+def main():
+    """Load in the masses, calculate their fuel and sum
+    """
+    masses = load_masses()
+    fuel_sum = np.sum(
+        [calculate_fuel(mass) for mass in masses]
+    )
+    print(fuel_sum)
+    
+if __name__ == '__main__':
+    main()
---
+++ day1/fuel_sum2.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def calculate_fuel(mass):
+    """Divide mass by three, round down and subtract 2
+    """
+    return int(mass / 3) - 2
+
+def load_masses(path='input.dat'):
+    return np.loadtxt(path)
+    
+def main():
+    """Load in the masses, calculate their fuel and sum
+    """
+    masses = load_masses()
+    total_fuel = 0
+    
+    for mass in masses:
+        fuel = calculate_fuel(mass)
+
+        while fuel > 0:
+            total_fuel += fuel
+            fuel = calculate_fuel(fuel)
+
+    print(total_fuel)
+    
+if __name__ == '__main__':
+    main()
