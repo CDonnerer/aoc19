bf71a020b4622ca03bc4a1ee02e8b4b5246719e9
Christian Donnerer <christian.donnerer@gmail.com>
2019-12-01 22:31:13 +0000

    refactored into single script to avoid DRY

--- day1/fuel_sum.py
+++ day1/fuel_sum.py
@@ -1,4 +1,11 @@
+"""Day 1 of the Advent of Code 2019 challenge"""
 import numpy as np
+
+
+def load_masses(path="input.dat"):
+    """TODO: fixed paths are not a good idea
+    """
+    return np.loadtxt(path)
 
 
 def calculate_fuel(mass):
@@ -7,16 +14,32 @@ def calculate_fuel(mass):
     return int(mass / 3) - 2
 
 
-def load_masses(path="input.dat"):
-    return np.loadtxt(path)
+def part_one(masses):
+    """Caclculate the masses for part one of the challenge
+    """
+    return np.sum([calculate_fuel(mass) for mass in masses])
+
+
+def part_two(masses):
+    """Caclculate the masses for part two of the challenge
+    """
+    total_fuel = 0
+
+    for mass in masses:
+        fuel = calculate_fuel(mass)
+
+        while fuel > 0:
+            total_fuel += fuel
+            fuel = calculate_fuel(fuel)
+    return total_fuel
 
 
 def main():
-    """Load in the masses, calculate their fuel and sum
+    """Load in the masses and Caclculate part one and part two
     """
     masses = load_masses()
-    fuel_sum = np.sum([calculate_fuel(mass) for mass in masses])
-    print(fuel_sum)
+    print(f"Answer to part one = {part_one(masses)}")
+    print(f"Answer to part two = {part_two(masses)}")
 
 
 if __name__ == "__main__":
